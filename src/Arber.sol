e03fbc9faa83574d2c71463f5008c30eea1ce622
JayArrowz <arrowzflame321@gmail.com>
2025-03-06 23:04:42 +0000

    Update Arber.sol

--- src/Arber.sol
+++ src/Arber.sol
@@ -9,10 +9,12 @@ import "@openzeppelin-contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract ArberUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable {
-
     using EnumerableSet for EnumerableSet.AddressSet;
 
     EnumerableSet.AddressSet private routers;
+
+    uint256 public slippageTolerance; // e.g., 50 = 0.5%
+    uint256 public estimatedGasCost; // approximate gas cost in wei per trade
 
     struct TokenPair {
         address token0;
@@ -21,40 +23,46 @@ contract ArberUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable 
 
     TokenPair[] public pairList;
 
-    function initialize(address[] memory _routers) public initializer {
+    function initialize(address[] memory _routers, uint256 _slippageTolerance, uint256 _estimatedGasCost)
+        public
+        initializer
+    {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
 
-        for(uint i = 0; i < _routers.length; i++) {
+        slippageTolerance = _slippageTolerance;
+        estimatedGasCost = _estimatedGasCost;
+
+        for (uint256 i = 0; i < _routers.length; i++) {
             routers.add(_routers[i]);
         }
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
-    function executeArbitrage(
-        address token0,
-        address token1,
-        uint amountIn
-    ) public onlyOwner returns(bool) {
-        uint bestPrice;
-        uint worstPrice = type(uint).max;
+    function executeArbitrage(address token0, address token1, uint256 amountIn) public onlyOwner returns (bool) {
+        uint256 bestPrice;
+        uint256 worstPrice = type(uint256).max;
         address bestRouter;
         address worstRouter;
 
-        for(uint i = 0; i < routers.length(); i++) {
-            uint amountOut = getAmountsOut(routers.at(i), token0, token1, amountIn);
-            if(amountOut > bestPrice) {
+        for (uint256 i = 0; i < routers.length(); i++) {
+            uint256 amountOut = getAmountsOut(routers.at(i), token0, token1, amountIn);
+            if (amountOut > bestPrice) {
                 bestPrice = amountOut;
                 bestRouter = routers.at(i);
             }
-            if(amountOut < worstPrice) {
+            if (amountOut < worstPrice) {
                 worstPrice = amountOut;
                 worstRouter = routers.at(i);
             }
         }
 
-        if(bestPrice <= worstPrice) {
+        uint256 minBestPrice = bestPrice - ((bestPrice * slippageTolerance) / 10000);
+        uint256 maxWorstPrice = worstPrice + ((worstPrice * slippageTolerance) / 10000);
+
+        uint256 profit = minBestPrice > maxWorstPrice ? minBestPrice - maxWorstPrice : 0;
+        if (profit <= estimatedGasCost) {
             return false;
         }
 
@@ -65,8 +73,8 @@ contract ArberUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable 
         path[0] = token0;
         path[1] = token1;
 
-        uint[] memory amountsReceived = IUniswapV2Router02(worstRouter).swapExactTokensForTokens(
-            amountIn, 0, path, address(this), block.timestamp
+        uint256[] memory amountsReceived = IUniswapV2Router02(worstRouter).swapExactTokensForTokens(
+            amountIn, maxWorstPrice, path, address(this), block.timestamp
         );
 
         IERC20(token1).approve(bestRouter, amountsReceived[1]);
@@ -75,29 +83,40 @@ contract ArberUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable 
         path[1] = token0;
 
         IUniswapV2Router02(bestRouter).swapExactTokensForTokens(
-            amountsReceived[1], 0, path, msg.sender, block.timestamp
+            amountsReceived[1], minBestPrice, path, msg.sender, block.timestamp
         );
 
         return true;
     }
 
-    function iteratePairList(uint start, uint n, uint amountIn) external onlyOwner {
-        uint end = start + n;
-        if(end > pairList.length) {
-            end = pairList.length;
-        }
+    function iteratePairList(uint256 start, uint256 n, uint256 amountIn) external onlyOwner {
+        uint256 len = pairList.length;
+        require(len > 0, "pairList is empty");
 
-        for(uint i = start; i < end; i++) {
-            executeArbitrage(pairList[i].token0, pairList[i].token1, amountIn);
+        for (uint256 i = 0; i < n; i++) {
+            uint256 index = (start + i) % len;
+            executeArbitrage(pairList[index].token0, pairList[index].token1, amountIn);
         }
     }
 
-    function getAmountsOut(address router, address tokenIn, address tokenOut, uint amountIn) public view returns(uint) {
+    function getAmountsOut(address router, address tokenIn, address tokenOut, uint256 amountIn)
+        public
+        view
+        returns (uint256)
+    {
         address[] memory path = new address[](2);
         path[0] = tokenIn;
         path[1] = tokenOut;
-        uint[] memory amounts = IUniswapV2Router02(router).getAmountsOut(amountIn, path);
+        uint256[] memory amounts = IUniswapV2Router02(router).getAmountsOut(amountIn, path);
         return amounts[1];
+    }
+
+    function setSlippageTolerance(uint256 _slippageTolerance) external onlyOwner {
+        slippageTolerance = _slippageTolerance;
+    }
+
+    function setEstimatedGasCost(uint256 _estimatedGasCost) external onlyOwner {
+        estimatedGasCost = _estimatedGasCost;
     }
 
     function addRouter(address router) external onlyOwner {
